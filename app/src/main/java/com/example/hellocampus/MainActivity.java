d9d9a45f53dca2d2b82f11fe9bd1257b69b06db1
Janssen Oseo <16400069@usc.edu.ph>
2026-03-05 03:01:00 +0800

    2nd Commit for Buttons working and Counter Increasing, file path for video is app/src/main/res/xml/Buttons working and Counter Increasing.mp4

--- app/src/main/java/com/example/hellocampus/MainActivity.java
+++ app/src/main/java/com/example/hellocampus/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         changeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                departmentText.setText("Dr. Name of Department Chairwoman");
+                departmentText.setText("Dr. Angie Ceniza Canillo");
             }
         });
 
